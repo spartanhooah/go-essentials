ec6c524eab8eaf7623fced8d10a2284bb426df81
Ben Frey <spartanhooah@gmail.com>
2024-08-18 09:59:59 -0400

    functions

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,30 +5,41 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount, years, expectedReturnRate float64
+	// var investmentAmount, years, expectedReturnRate float64
 	// var investmentAmount float64 = 1000
 	// years := 10.0
 	// expectedReturnRate := 5.5
 	// Could combine the above three lines like the below; stylistic choice
 	// investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
-	fmt.Print("Initial investment amount: ")
-	fmt.Scan(&investmentAmount)
+	investmentAmount := prompt("Initial investment amount: ")
+	expectedReturnRate := prompt("Expected return rate: ")
+	years := prompt("Length of the investment in years: ")
 
-	fmt.Print("Expected return rate: ")
-	fmt.Scan(&expectedReturnRate)
-
-	fmt.Print("Length of the investment in years: ")
-	fmt.Scan(&years)
-
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formattedFV := fmt.Sprintf("Future value without inflation: %.2f\n", futureValue)
 	fmt.Print(formattedFV)
 	fmt.Printf("Future value after inflation: %.2f", futureRealValue)
 
 	// Can use back-ticks for multi-line strings. In this case, \n is printed as \n rather than a line break
+}
+
+func prompt(prompt string) float64 {
+	fmt.Print(prompt)
+
+	var input float64
+	fmt.Scan(&input)
+
+	return input
+}
+
+func calculateFutureValues(investment, returnRate, years float64) (float64, float64) {
+	fv := investment * math.Pow(1+returnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv, rfv
 }
